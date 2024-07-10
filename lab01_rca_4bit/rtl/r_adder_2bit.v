2f87d581dc9e830ec678f4e51e80f69d76214fd1
hee_dusu <132325298+heeesoo@users.noreply.github.com>
2024-07-10 21:38:49 +0900

    Update r_adder_2bit.v

--- lab01_rca_4bit/rtl/r_adder_2bit.v
+++ lab01_rca_4bit/rtl/r_adder_2bit.v
@@ -1,26 +1,26 @@
 module r_adder_2bit(
 	input	[1:0]	a,
 	input	[1:0]	b,
-	input			c,
+	input		c,
 
 	output	[1:0]	out,
-	output			cout
+	output		cout
 );
 
 wire	co_w;
 
 full_adder u_full_adder_1(
-	.a		(a[0]),
-	.b		(b[0]),
-	.c		(c),
+	.a	(a[0]),
+	.b	(b[0]),
+	.c	(c),
 	.out	(out[0]),
 	.cout	(co_w)
 );
 
 full_adder u_full_adder_2(
-	.a		(a[1]),
-	.b		(b[1]),
-	.c		(co_w),
+	.a	(a[1]),
+	.b	(b[1]),
+	.c	(co_w),
 	.out	(out[1]),
 	.cout	(cout)
 );
