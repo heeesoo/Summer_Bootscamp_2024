d677481cca2eef058e23e9b57b2ff4a0423d5e9f
hee_dusu <132325298+heeesoo@users.noreply.github.com>
2024-07-10 21:37:41 +0900

    Delete linux_development/lab01_rca_4bit directory

--- linux_development/lab01_rca_4bit/sim/run.f
+++
@@ -1,5 +0,0 @@
-../../lab01_rca_4bit/rtl/r_adder_4bit.v
-../../lab01_rca_4bit/rtl/r_adder_2bit.v
-../../lab01_rca_4bit/rtl/full_adder.v
-../../lab01_rca_4bit/rtl/half_adder.v
-../../lab01_rca_4bit/sim/tb_adder.v
